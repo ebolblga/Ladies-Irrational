ebb7ef5680e572146a2f48c1ba895a2ed16e5125
ebolblga <kkochanovskiy@gmail.com>
2022-04-19 21:07:23 +0300

    Add project files.

---
+++ Ladies Irrational/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    string pathA = "..\\a.txt";
+    string pathB = "..\\b.txt";
+    ifstream fileA;
+    fileA.open(pathA);
+   
+    string line;
+
+    while (getline(fileA, line))
+    {
+        ifstream fileB;
+        fileB.open(pathB);
+        string line2;
+
+        while (getline(fileB, line2))
+            cout << line << "\t" << line2 << '\n';
+    }
+    cout << endl;
+}
